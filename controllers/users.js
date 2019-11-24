69d72ef8271b71b86b158e2c1ca6db0522f40901
Tuomas Välimäki <t.valimaki@gmail.com>
2019-11-24 17:37:28 +0200

    convert user controller to use async/await

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,19 @@ const bcryptjs = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/', (_request, response) => {
+/*usersRouter.get('/', (_request, response) => {
   User.find({}).then(persons => {
     response.json(persons.map(person => person.toJSON()))
   })
+})*/
+
+usersRouter.get('/', async (_request, response, next) => {
+  try {
+    const users = await User.find({})
+    response.json(users.map(user => user.toJSON()))
+  } catch (error) {
+    next(error)
+  }
 })
 
 usersRouter.post('/', async (request, response, next) => {
